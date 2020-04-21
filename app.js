8ab136675b1f0be4565c15bae3b6b8fefba23a3a
Supreme1755 <63268196+Supreme1755@users.noreply.github.com>
2020-04-20 21:22:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $ = jQuery;
 var resistencia = $("#resistencia")[0].innerText
 var resrac = $("#resrac")[1].innerText
-var resexo = $("#resexo")[1].innerText
+var resexo = $("#resexo")[0].innerText
 var total = resistencia + resrac + resexo
-var maxHealth = 10*resrac,
+var maxHealth = 10*resistencia,
   curHealth = maxHealth;
 $('.total').html(maxHealth + "/" + maxHealth);
 $(".health-bar-text").html("100%");
